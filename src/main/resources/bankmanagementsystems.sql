1a263f7ad2ecff5ef4c6ea955e0cb16717214a34
Mali Prakash Datta <152262211+maliprakashdatta@users.noreply.github.com>
2024-08-07 12:51:58 +0530

    Update bankmanagementsystems.sql

--- src/main/resources/bankmanagementsystems.sql
+++ src/main/resources/bankmanagementsystems.sql
@@ -5,7 +5,7 @@ CREATE TABLE bankmanagementsystems.card (
   cardId INT NOT NULL,
   cardNo INT NOT NULL,
   accountNo INT NULL,
-  accountHolderName` VARCHAR(45) NULL,
+  accountHolderName VARCHAR(45) NULL,
   cvv INT NULL,
   cardType VARCHAR(45) NULL,
   PRIMARY KEY (cardId, cardNo));
