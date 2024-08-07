e21c78ccb1bbcada0e7c440e4b0bcc8ccd948bc2
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-08-07 10:45:12 +0530

    commit Connection service

--- src/main/java/com/swsa/service/ConnectionService.java
+++ src/main/java/com/swsa/service/ConnectionService.java
@@ -9,8 +9,7 @@ public class ConnectionService
         Connection connection = null;
         try {
 
-            String url = "jdbc:mysql://localhost:3306/student";
-          //  String url = "jdbc:mysql://localhost:3306/ssbms";
+            String url = "jdbc:mysql://localhost:3306/bankmanagementsystems";
             String username = "root";
             String password = "Prakash@123";
             connection = DriverManager.getConnection(url, username, password);
