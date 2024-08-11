7c278bd190a5c1c57c09a8fb8a0495bb8bcd5d77
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-08-11 19:35:09 +0530

    service

--- src/main/java/com/swsa/service/AccountService.java
+++ src/main/java/com/swsa/service/AccountService.java
@@ -9,9 +9,8 @@ public class AccountService {
     private static final AccountRepository ACCOUNT_REPOSITORY = new AccountRepository();
 
 
-    public static void insertCustomerAccount()
-    {
-        Scanner scanner=new Scanner(System.in);
+    public static void insertCustomerAccount() {
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Enter account number: ");
         String accountNumber = scanner.nextLine();
 
@@ -19,10 +18,10 @@ public class AccountService {
         String accountHolderName = scanner.nextLine();
 
         System.out.print("Enter initial deposit amount: ");
-        double balance =scanner.nextDouble();
+        double balance = scanner.nextDouble();
 
         scanner.nextLine();  // Consume newline
-        Account account1=new Account(accountNumber,accountHolderName,balance);
+        Account account1 = new Account(accountNumber, balance);
 
         try {
             if (ACCOUNT_REPOSITORY.insertCustomerAccount(account1)) {
@@ -30,60 +29,68 @@ public class AccountService {
             } else {
                 System.out.println(" Account not Open .");
             }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+    public static void  depositMoney()
+    {
+        double amount;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter account number: ");
+        String accountNumber = scanner.nextLine();
+
+        System.out.print("Enter deposit amount: ");
+        amount = scanner.nextDouble();
+
+        scanner.nextLine();  // Consume newline
+        Account account = new Account(accountNumber,amount);
+        try {
+            if (ACCOUNT_REPOSITORY.depositMoney(account)) {
+                System.out.println(" Deposit Amount  successfully!");
+            } else {
+                System.out.println(" Account not Open .");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
+/*
+
+    private static void withdrawMoney(double amount)
+    {
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter account number: ");
+        String accountNumber = scanner.nextLine();
+        System.out.print("Enter withdrawal amount: ");
+        amount = scanner.nextDouble();
+        scanner.nextLine();  // Consume newline
+        Account account2=new Account(accountNumber,amount);
+        try {
+            if (ACCOUNT_REPOSITORY.withdrawMoney(account2)) {
+                System.out.println("  withdraw Money  successfully!");
+            } else {
+                System.out.println("Not withdraw Money  .");
+            }
         } catch (SQLException e)
         {
             throw new RuntimeException(e);
         }
 
-    }}
-//
-//    private static void depositMoney(double amount)
-//    {
-//        Scanner scanner=new Scanner(System.in);
-//        System.out.print("Enter account number: ");
-//        String accountNumber = scanner.nextLine();
-//        System.out.print("Enter deposit amount: ");
-//        amount = scanner.nextDouble();
-//        scanner.nextLine();  // Consume newline
-//        Account account=new Account(accountNumber,amount);
-//        try {
-//            if (ACCOUNT_REPOSITORY.depositMoney( account)) {
-//                System.out.println(" Deposit Amount  successfully!");
-//            } else {
-//                System.out.println(" Account not Open .");
-//            }
-//        } catch (SQLException e)
-//        {
-//            throw new RuntimeException(e);
-//        }
-//
-//    }
-//
-//    private static void withdrawMoney(double amount)
-//    {
-//        Scanner scanner=new Scanner(System.in);
-//        System.out.print("Enter account number: ");
-//        String accountNumber = scanner.nextLine();
-//        System.out.print("Enter withdrawal amount: ");
-//        amount = scanner.nextDouble();
-//        scanner.nextLine();  // Consume newline
-//        Account account2=new Account(accountNumber,amount);
-//        try {
-//            if (ACCOUNT_REPOSITORY.withdrawMoney(account2)) {
-//                System.out.println("  withdraw Money  successfully!");
-//            } else {
-//                System.out.println("Not withdraw Money  .");
-//            }
-//        } catch (SQLException e)
-//        {
-//            throw new RuntimeException(e);
-//        }
-//
-//    }
-//
-//
-//
-//}
+    }
+*/
+
+
+
+
+
 
 
 
