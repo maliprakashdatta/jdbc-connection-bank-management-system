c88b00081ac5938c8d44768fe2f3afdc20487302
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-08-15 12:34:02 +0530

    committed

--- src/main/java/com/swsa/repository/AccountRepository.java
+++ src/main/java/com/swsa/repository/AccountRepository.java
@@ -79,39 +79,6 @@ public class AccountRepository {
         return false;
     }
 
-    public Account retrieveAccount() {
-        Account account = null;
-        // Use the connection to execute SQL queries and interact with the database
-        try {
-            this.initConnection();
-            // Your database operations here...
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM account where accountNumber = " + account);
-            // Iterate over the result set
-            while (resultSet.next()) {
-
-                String accountNumber = resultSet.getString("AccountNumber");
-                String accountHolderName = resultSet.getString("AccountHolderName");
-                double balance = resultSet.getLong("balance");
-                int customerId = resultSet.getInt("customerId");
-
-                // Do something with the data, e.g., print it
-                account = new Account(accountNumber, accountHolderName, balance, customerId);
-            }
-        } catch (SQLException e) {
-            System.err.println("SQL error: " + e.getMessage());
-        } finally {
-            // Close the connection when done
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    System.err.println("Error closing connection: " + e.getMessage());
-                }
-            }
-        }
-        return account;
-    }
 
 
     // Method to update user data into the database
@@ -150,52 +117,6 @@ public class AccountRepository {
 
 
 
-
-
-/*
-
-    public List<Account> retrievedeposit()
-
-    {
-        List<Account> accounts = new ArrayList<>();
-
-        // Use the connection to execute SQL queries and interact awith the database
-        try {
-            this.initConnection();
-
-            // Your database operations here...
-            Statement statement = connection.createStatement();
-            ResultSet resultSet1 = statement.executeQuery("SELECT * FROM account");
-
-            // Iterate over the result set
-            while (ResultSet.next())
-            {
-                String accountNumber = resultSet1.getString("AccountNumber");
-                String accountHolderName = resultSet1.getString("AccountHolderName");
-                double balance = resultSet1.getLong("balance");
-                int customerId = resultSet1.getInt("customerId");
-
-                // Do something with the data, e.g., print it
-                Account account = new Account(accountNumber, balance);
-                accounts.add(account);
-            }
-        } catch (SQLException e) {
-            System.err.println("SQL error: " + e.getMessage());
-        } finally {
-            // Close the connection when done
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    System.err.println("Error closing connection: " + e.getMessage());
-                }
-            }
-        }
-        return accounts;
-    }
-*/
-
-
     // Method to update user data into the database
     public boolean insertdepositMoney(Account account) throws SQLException {
         this.initConnection();
@@ -231,7 +152,7 @@ public class AccountRepository {
         public boolean updatedepositMoney(Account account) throws SQLException {
             this.initConnection();
 
-            String query = "UPDATE account  SET   AccountNumber= ?,AccountHolderName(),Balance = Amount +  ?,CustomerId=?";
+            String query = "UPDATE account  SET   AccountNumber= ?,AccountHolderName(),Balance = Amount +  ?, CustomerId=?";
             try (PreparedStatement preparedStatement = connection.prepareStatement(query))
             {
                 preparedStatement.setString(1, account.getAccountNumber());
@@ -252,43 +173,6 @@ public class AccountRepository {
 
 
 
-
-
-    public Account retrieverDeposit( String accountNumber) {
-        Account account = null;
-        // Use the connection to execute SQL queries and interact with the database
-        try {
-            this.initConnection();
-            // Your database operations here...
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM account where accountNumber = " +  accountNumber);
-            // Iterate over the result set
-            while (resultSet.next()) {
-
-                accountNumber = resultSet.getString("AccountNumber");
-                String accountHolderName = resultSet.getString("AccountHolderName");
-                double balance = resultSet.getLong("balance");
-                int customerId = resultSet.getInt("customerId");
-
-                // Do something with the data, e.g., print it
-                account = new Account(accountNumber, balance,accountHolderName,customerId);
-            }
-        } catch (SQLException e) {
-            System.err.println("SQL error: " + e.getMessage());
-        } finally {
-            // Close the connection when done
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    System.err.println("Error closing connection: " + e.getMessage());
-                }
-            }
-        }
-        return account;
-    }
-
-
     //================================================================
     //---------------Withdraw Amount -----------------------------
   //====================================================================
@@ -299,14 +183,48 @@ public class AccountRepository {
     // Method to update user data into the database
     public boolean insertwithdrawMoney(Account account) throws SQLException {
         this.initConnection();
-        String query = "INSERT INTO account VALUES (?, ?)";
+        int amount = 0;
+        String query = "INSERT INTO account VALUES (?, ?, ?, ?)";
         try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
 
             preparedStatement.setString(1, account.getAccountNumber());
-            preparedStatement.setDouble(2, account.getBalance());
-            // preparedStatement.setDouble(4, account.getCustomer().getCustomerId());
+            preparedStatement.setString(2, account.getAccountHolderName());
+            preparedStatement.setDouble(3, account.getBalance());
+            preparedStatement.setInt(4, account.getCustomer().getCustomerId());
+            System.out.println("  Account created  data successfully .. : " + account);
 
             System.out.println("Withdraw Amount successfully .. : " + account);
+/*
+            if (amount > 0 && account.getBalance() >= amount) {
+                account.setBalance(account.getBalance() - amount);
+                System.out.println("Withdrew $" + amount + " from account " + account.getAccountNumber());
+            } else if (amount > 0 && account.getBalance() < amount) {
+                System.out.println("Insufficient funds for withdrawal.");
+            } else {
+                System.out.println("Withdrawal amount must be positive.");
+            }*/
+            int rowsInserted = preparedStatement.executeUpdate();
+
+            return rowsInserted > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    // Method to update user data into the database
+    public boolean updatewithdrawMoney(Account account) throws SQLException {
+        this.initConnection();
+
+        String query = "UPDATE account  SET   AccountNumber= ?, AccountHolderName(),Balance = Amount -  ?, CustomerId=?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query))
+        {
+            preparedStatement.setString(1, account.getAccountNumber());
+            preparedStatement.setString(2, account.getAccountHolderName());
+            preparedStatement.setDouble(3, account.getBalance());
+            preparedStatement.setDouble(4, account.getCustomer().getCustomerId());
+
+            System.out.println("Withdraw Money Sucessfully........: " + account);
 
             int rowsInserted = preparedStatement.executeUpdate();
 
@@ -316,6 +234,7 @@ public class AccountRepository {
         }
         return false;
     }
+
 
 
 /*
@@ -341,6 +260,39 @@ public class AccountRepository {
     }
 */
 
+    public Account retrieveAccount() {
+        Account account = null;
+        // Use the connection to execute SQL queries and interact with the database
+        try {
+            this.initConnection();
+            // Your database operations here...
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM account where accountNumber = " + account);
+            // Iterate over the result set
+            while (resultSet.next()) {
+
+                String accountNumber = resultSet.getString("AccountNumber");
+                String accountHolderName = resultSet.getString("AccountHolderName");
+                double balance = resultSet.getLong("balance");
+                int customerId = resultSet.getInt("customerId");
+
+                // Do something with the data, e.g., print it
+                account = new Account(accountNumber, accountHolderName, balance, customerId);
+            }
+        } catch (SQLException e) {
+            System.err.println("SQL error: " + e.getMessage());
+        } finally {
+            // Close the connection when done
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    System.err.println("Error closing connection: " + e.getMessage());
+                }
+            }
+        }
+        return account;
+    }
 
 }
 
